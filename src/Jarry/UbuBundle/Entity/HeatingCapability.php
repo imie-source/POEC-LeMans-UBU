95532ba04cbce24bd3f4e95f24f607c390e6c2eb
David Girault <davidpaul.musique@free.fr>
2016-01-27 15:35:18 +0100

    On avance -> HeatingCapability

--- src/Jarry/UbuBundle/Entity/HeatingCapability.php
+++ src/Jarry/UbuBundle/Entity/HeatingCapability.php
@@ -3,14 +3,35 @@
 namespace Jarry\UbuBundle\Entity;
 
 use Jarry\UbuBundle\Entity\Capability;
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * HeatingCapability
+ * @ORM\Entity
+ * @ORM\Table(name='log_heating')
  */
 class HeatingCapability extends Capability
 {
-    private $nameOfTable = 'log_heating';
+    //private $nameOfTable = 'log_heating';
     
+    // ne pas rater la méthode qui permet de gérer l'id
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     */
+    private $id;
+    
+    /**
+     *@ORM\Column(type="float")
+     */
     private $tempActor;
+    
+    /**
+     *@ORM\Column(type="float")
+     */
     private $tempTargetSensor;
+    
+    /**
+     *@ORM\Column(type="float")
+     */
     private $tempEnvSensor;
 }
