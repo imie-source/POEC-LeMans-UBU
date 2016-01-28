f486eb3c2b98548453fb7ff5a4b08cd276aada03
David Girault <davidpaul.musique@free.fr>
2016-01-28 16:04:46 +0100

    Fixtures pour User / Zone /Place

--- src/Jarry/UbuBundle/DataFixtures/ORM/LoadZoneData.php
+++ src/Jarry/UbuBundle/DataFixtures/ORM/LoadZoneData.php
@@ -1,6 +1,6 @@
 <?php
 
-/*namespace Jarry\UbuBundle\DataFixtures\ORM;
+namespace Jarry\UbuBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
@@ -8,13 +8,36 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
 use Jarry\UbuBundle\Entity\Zone;
 
-class LoadPlaceData extends AbstractFixture implements OrderedFixtureInterface
+class LoadZoneData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $em) {
-        $places = ['maison', 'bicoque', 'bidonville','chateau', 'manoir', 'villa', 'residence'];
+        $places = ['maison', 'bicoque', 'bidonville','lupanard' ,'chateau', 'manoir', 'villa', 'residence'];
+        $zones = ['chambre', 'cuisine', 'séjour', 'couloir', 'salle_de_bain'];
+        foreach ($places as $placeName) {
+            foreach ($zones as $zoneName) {
+                $entity = new Zone();
+                $entity->setPlace($em->merge($this->getReference('place-'.$placeName)));
+                $entity->setName($zoneName);
+                $entity->setInOutStatus(FALSE);
+                $entity->setPostLat(2.25);
+                $entity->setPostLong(5.75);
+                $entity->setPosAlt(4.05);
+                $entity->setOctoX(0.75);
+                $entity->setOctoY(0.35);
+                $entity->setOctoZ(78.3);
+                
+                $em->persist($entity);
+                
+                $this->addReference('zone'.$placeName.'-'.$zoneName, $entity);
+                
+                
+            }
+            
+        }
+        $em->flush();
     }
     
     public function getOrder() {
         return 300;
     }
-}*/
+}
