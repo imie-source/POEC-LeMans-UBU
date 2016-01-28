99c4480304e430dae5f2706581da892108333024
David Girault <davidpaul.musique@free.fr>
2016-01-28 11:08:23 +0100

    debut travail sur les fixtures bis

---
+++ src/Jarry/UbuBundle/DataFixtures/ORM/LoadZoneData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jarry\UbuBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Jarry\UbuBundle\Entity\Zone;
+
+class LoadPlaceData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $em) {
+        $places = ['maison', 'bicoque', 'bidonville','chateau', 'manoir', 'villa', 'residence'];
+    }
+    
+    public function getOrder() {
+        return 300;
+    }
+}
