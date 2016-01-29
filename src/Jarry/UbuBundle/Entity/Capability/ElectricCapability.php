0d1a17959010e3ffbbf888f1c88552bf81986057
David Girault <davidpaul.musique@free.fr>
2016-01-29 14:13:14 +0100

    Reclassage des dossiers et création des logs

---
+++ src/Jarry/UbuBundle/Entity/Capability/ElectricCapability.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Jarry\UbuBundle\Entity\Capability;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Events;
+use Jarry\UbuBundle\Entity\MainCapability;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="log_electric")
+ */
+class ElectricCapability extends MainCapability
+{
+    
+    /**
+     *@ORM\Column(type="boolean")
+     */
+    private $onOffActor;
+    
+    /**
+     *@ORM\Column(type="float")
+     */
+    private $powerSensor;
+
+    /**
+     * Set onOffActor
+     *
+     * @param boolean $onOffActor
+     *
+     * @return ElectricCapability
+     */
+    public function setOnOffActor($onOffActor)
+    {
+        $this->onOffActor = $onOffActor;
+
+        return $this;
+    }
+
+    /**
+     * Get onOffActor
+     *
+     * @return boolean
+     */
+    public function getOnOffActor()
+    {
+        return $this->onOffActor;
+    }
+
+    /**
+     * Set powerSensor
+     *
+     * @param float $powerSensor
+     *
+     * @return ElectricCapability
+     */
+    public function setPowerSensor($powerSensor)
+    {
+        $this->powerSensor = $powerSensor;
+
+        return $this;
+    }
+
+    /**
+     * Get powerSensor
+     *
+     * @return float
+     */
+    public function getPowerSensor()
+    {
+        return $this->powerSensor;
+    }
+}
