dafb51b42dcfacd25c06e19209a7df6b41224b15
David Girault <davidpaul.musique@free.fr>
2016-02-01 10:31:32 +0100

    Ajout de quelques utilisateurs et toutes les places ont leur owner

--- src/Jarry/UbuBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Jarry/UbuBundle/DataFixtures/ORM/LoadUserData.php
@@ -26,14 +26,28 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         $jac->setLogin('jacky');
         $jac->setPassword('michel');
         
+        $val = new User();
+        $val->setUsername('Valentin Provost');
+        $val->setPassword('provost');
+        $val->setLogin('valentin');
+        
+        $dav = new User();
+        $dav->setUsername('David Girault');
+        $dav->setLogin('david');
+        $dav->setPassword('girault');
+        
         $em->persist($root);
         $em->persist($first);
+        $em->persist($val);
+        $em->persist($dav);
         
         $em->flush();
         
         $this->addReference('user-root', $root);
         $this->addReference('user-first', $first);
         $this->addReference('user-jac', $jac);
+        $this->addReference('user-val', $val);
+        $this->addReference('user-dav', $dav);
     }
     
     public function getOrder() {
