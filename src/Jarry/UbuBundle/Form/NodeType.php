1ace758209b2df970ba568dcf33676b131ce559d
David Girault <davidpaul.musique@free.fr>
2016-02-19 11:05:21 +0100

    Merge origin/dev into dev
    Conflicts:
    	src/Jarry/UbuBundle/Resources/views/masterlayout.html.twig

--- src/Jarry/UbuBundle/Form/NodeType.php
+++ src/Jarry/UbuBundle/Form/NodeType.php
@@ -5,6 +5,7 @@ namespace Jarry\UbuBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class NodeType extends AbstractType
 {
@@ -17,6 +18,26 @@ class NodeType extends AbstractType
         $builder
             ->add('name')
             ->add('ip')
+            ->add('nodeType', ChoiceType::class,
+                    
+                    array('label'=>'Type de node',
+                        'choices' => array(
+                        'rad'=>'Radiateur',
+                        'lum'=>'Lumière',
+                        'sto'=>'Store / Volet electrique',
+                        'rob'=>'Robot',
+                        'cam'=>'Caméra',
+                        'tvo'=>'Télévision',
+                        'met'=>'Station météo',
+                        'asp'=>'Aspirateur',
+                        'the'=>'Thermomètre',
+                        'mic'=>'Microphone',
+                        'cub'=>'Cubase',
+                        'hps'=>'Enceinte / Chaine HiFi',)
+                    ))
+                    
+                    
+                
             //->add('zone')
         ;
     }
